d86df43f2f31c65d77b6b0f28721a5fb2756b822
Bohdan Ptyts <bohdanptyts@gmail.com>
2024-07-16 23:09:35 +0200

    Update ESLint

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+// @ts-expect-error
+import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), eslint()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
